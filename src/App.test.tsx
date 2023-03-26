d2b4fa8a97b0ba3e9f01ddac7ad1d9b7c681c411
Tai Vo <tantaivo74@gmail.com>
2023-03-26 21:15:59 +0700

    feat: add first unit test

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import App from 'App'
+import { describe, it } from 'vitest'
+
+describe('App', () => {
+  it('Render title of app', () => {
+    // ARRANGE
+    render(<App />)
+    // ACT
+    // EXPECT
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Vite + Reactzzz')
+  })
+})
