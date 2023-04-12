3c0ae17770b3d45542f9801ab7524525207c5680
Tai Vo <tantaivo74@gmail.com>
2023-04-12 22:08:23 +0700

    chore: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,16 +27,18 @@ module.exports = {
     'jest-dom'
   ],
   rules: {
+    'linebreak-style': 0,
     'react/react-in-jsx-scope': 0,
     'import/prefer-default-export': ['off', { target: 'any' }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'import/no-cycle': 0,
     'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
     '@typescript-eslint/semi': ['error', 'never'],
     '@typescript-eslint/comma-dangle': ['error', 'never'],
     '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-magic-numbers': ['error', { ignore: [0, 1] }],
     '@typescript-eslint/no-explicit-any': 'error'
   }
 }
