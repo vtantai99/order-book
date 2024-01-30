eb6558a275246729bfd81665ad383796ff9cc3b3
Tai Vo <tantaivo74@gmail.com>
2024-01-30 16:27:55 +0700

    chore: setup redux, redux-toolkit

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,1 +1,11 @@
-export {}
+import { configureStore } from '@reduxjs/toolkit'
+import orderBookReducer from 'components/OrderBook/orderBookSlice'
+
+export const store = configureStore({
+  reducer: {
+    orderbook: orderBookReducer
+  }
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
